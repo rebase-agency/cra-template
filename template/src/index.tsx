4d83327aba2b78c76566e89d74a921af0a63ea5a
Victoria Kapitonenko <v.kapitonenko@rebase.agency>
2023-10-23 16:37:28 +0300

    feat: delete import react

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -2,7 +2,6 @@ import "normalize.css";
 
 import "./assets/css/global.css";
 
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
