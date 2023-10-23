2667c06fcc646646d221cfbcc40a6600788f0d75
Victoria Kapitonenko <v.kapitonenko@rebase.agency>
2023-10-23 16:32:47 +0300

    feat: change depenciences, fix lint

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import StartPage from "./pages";
 
 function App() {
--- template/src/pages/index.tsx
+++ template/src/pages/index.tsx
@@ -1,7 +1,3 @@
-import React from "react";
-
 export default function StartPage() {
-  return (
-    <div>Welcome to Rebase CRA Template</div>
-  );
+  return <div>Welcome to Rebase CRA Template</div>;
 }
